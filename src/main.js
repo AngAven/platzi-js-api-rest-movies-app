37b1f378b038645d11cef607d12a048ed8886ab5
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2024-09-02 08:40:09 -0600

    Using axios

--- src/main.js
+++ src/main.js
@@ -7,31 +7,76 @@ const options = {
     }
 }
 
-const createMovieContainer = (url, title, parentClass) => {
+const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        accept: 'application/json',
+        Authorization: BEREAR
+    }
+})
+
+const createContainer = (
+    url = '',
+    titleName = '',
+    parentClass = '',
+    containerClass = '',
+    id = ''
+) => {
     const parent = document.querySelector(parentClass)
     const container = document.createElement('div')
-    const img = document.createElement('img')
 
-    container.classList.add('movie-container')
-    img.classList.add('movie-img')
-    img.src = url
-    img.alt = title
-    container.appendChild(img)
-    parent.appendChild(container)
+    if(containerClass === 'movie-container'){
+        const img = document.createElement('img')
+
+        container.classList.add(containerClass)
+        img.classList.add('movie-img')
+        img.src = url
+        img.alt = titleName
+        container.appendChild(img)
+        parent.appendChild(container)
+    } else if (containerClass === 'category-container'){
+        const title = document.createElement('h3')
+
+        container.classList.add('category-container')
+        title.id = 'id' + id
+        title.classList.add('category-title')
+        title.textContent = titleName
+
+        container.appendChild(title)
+        parent.appendChild(container)
+    }
 
     return container
 }
 
 const getTrendingMoviesPreview = async () => {
-    const response = await fetch(`${BASE_URL}trending/movie/day?language=en-US`, options)
-    const {results} = await response.json()
+    const {data} = await api('trending/movie/day?language=en-US')
+    const {results} = data
+
     results.forEach((movie) => {
-        console.log(movie)
-        createMovieContainer(
+        createContainer(
             'https://image.tmdb.org/t/p/w300/' + movie.poster_path,
             movie.title,
-            '.trendingPreview-movieList')
+            '.trendingPreview-movieList',
+            'movie-container'
+        )
+    })
+}
+
+const getCategoriesPreview = async () => {
+    const {data} = await api('genre/movie/list?language=en')
+    const {genres} = data
+
+    genres.forEach(categorie => {
+        createContainer(
+            '',
+            categorie.name,
+            '.categoriesPreview-list',
+            'category-container',
+            categorie.id
+        )
     })
 }
 
 getTrendingMoviesPreview()
+getCategoriesPreview()
